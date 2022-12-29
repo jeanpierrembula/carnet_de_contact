e2cc2aa17dc9d85e8c9b413bd05aa0d4c831f2ac
zozer <18ml253@esisalama.org>
2022-12-29 17:20:34 +0200

    modified:   index.html 	modified:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,28 @@
+// Fonction pour ajouter un contact à l'annuaire
+
+function addContact(nom, postnom, tel, groupe, myemail, detail, myimage) {
+    // Creation de la structure html de la partie contact 
+    const htmlStructure = `
+    <div class="contact">
+      <img src="${myimage}" alt="Image de ${nom} ${postnom}">
+      <h3>${nom} ${postnom}</h3>
+      <p>${tel}</p>
+      <p>${groupe}</p>
+      <p>${myemail}</p>
+      <p>${detail}</p>
+      <button class="edit-button">Modifier</button>
+      <button class="delete-button">Supprimer</button>
+    </div>
+  `;
+
+  const contactsDiv = document.getElementById('contacts');
+  contactsDiv.innerHTML += contactHTML;
+  console.log(htmlStructure)
+}
+
+function getContactValue(){
+    const nom = document.getElementById("nom")
+    const postnom = document.getElementById("postnom")
+    const tel = document.getElementById("tel")
+    const groupe = document.getElementById("groupe")
+}
